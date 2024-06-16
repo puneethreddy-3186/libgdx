c6af0ed3878cc39a1cb327ca64baee324b7338ff
Puneeth Reddy <reachpuneethreddy@gmail.com>
2024-06-16 19:10:45 +0200

    Initial commit

---
+++ libgdx-demo-invaders-master/desktop/src/com/badlogic/invaders/desktop/DesktopLauncher.java
@@ -0,0 +1,15 @@
+package com.badlogic.invaders.desktop;
+
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
+import com.badlogic.invaders.Invaders;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+		config.setForegroundFPS(60);
+		config.setTitle("Invaders");
+		new Lwjgl3Application(new Invaders(), config);
+	}
+}
+
